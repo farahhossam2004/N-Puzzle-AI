298b9375976ec04efaaf22efb96bc2fbf12a68c1
Farah Hossam <88346628+farahhossam2004@users.noreply.github.com>
2024-08-12 13:23:07 +0300

    Basic GUI

---
+++ settings.py
@@ -0,0 +1,14 @@
+# Colors
+White = (255, 255, 255)
+Black = (0,0,0)
+brown = (70,38,11)
+LightGrey = (100,100,100)
+BGColor = brown
+
+# game settings
+width = 1110
+height = 641
+FPS = 60
+title = "N - Puzzle "
+tileSize = 120
+gameSize = 3
